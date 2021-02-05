e513bca619da28876b59503f5f16ac00a0c8e9d8
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2021-02-05 10:42:20 -0500

    Add User model with get and set

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World")
+import { User } from './models/User';
+
+const user = new User({ name: 'Kevin', age: 34 })
+
+console.log(user)
+console.log(user.get('name'))
+
+user.set({ name: "yo" })
+console.log(user.get('name'))
