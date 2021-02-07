07631e4a03d989b4b8a930ee6d84fe0aaf86f520
Kevin Robert Andrews <kevinrobertandrews@gmail.com>
2021-02-07 17:13:38 -0500

    Refactor attributes to use delegation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
 import { User } from './models/User';
 
-const user = new User({ name: "new guy", age: 0 })
-
-user.events.on('change', () => {
-    console.log('Change!!')
-})
-
+const user = new User({ name: "Kevin", age: 34 })
 console.log(user)
-
-user.events.trigger('change');
